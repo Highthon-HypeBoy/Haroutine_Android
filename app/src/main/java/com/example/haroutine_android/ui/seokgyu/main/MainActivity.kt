fc49a411fe3a798c149e1f00a5a24d8515c5026b
유현명 <aahspringaa4@naver.com>
2023-01-15 05:35:06 +0900

    ADD :: pin Notification

--- app/src/main/java/com/example/haroutine_android/ui/seokgyu/main/MainActivity.kt
+++ app/src/main/java/com/example/haroutine_android/ui/seokgyu/main/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.haroutine_android.ui.seokgyu.main
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.app.NotificationCompat
 import com.example.haroutine_android.R
 import com.example.haroutine_android.databinding.ActivityMainBinding
-import com.example.haroutine_android.dto.response.seokgyu.GetRoutineResponse
 import com.example.haroutine_android.ui.BaseActivity
 import com.example.haroutine_android.ui.hyunmyeong.routinedetail.NonRoutineDetailActivity
 import com.example.haroutine_android.ui.hyunmyeong.routinedetail.RoutineDetailActivity
-import com.example.haroutine_android.util.repeatOnStarted
+import com.example.haroutine_android.ui.notification.NotificationService
+
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel: MainViewModel by viewModels()
@@ -25,6 +31,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         binding.nonroutine.setOnClickListener {
             val intent = Intent(this, NonRoutineDetailActivity::class.java)
             startActivity(intent)
+        }
+        val intent = Intent(this@MainActivity, NotificationService::class.java)
+        intent.action = NotificationService.ACTIVITY_SERVICE
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            val CHANNEL_ID = "my_channel_01"
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
+                channel
+            )
+            startService(intent)
         }
     }
 
