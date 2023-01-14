dfce2b7ace68059d7a53a18cbbd05828d861f883
유현명 <aahspringaa4@naver.com>
2023-01-14 20:06:37 +0900

    ADD :: RoutineDetail Business

--- app/src/main/java/com/example/haroutine_android/repository/HyunMyeongRepository.kt
+++ app/src/main/java/com/example/haroutine_android/repository/HyunMyeongRepository.kt
@@ -1,4 +1,10 @@
 package com.example.haroutine_android.repository
 
-class HyunMyeongRepository {
+import com.example.haroutine_android.api.hyunMyeongApi
+import com.example.haroutine_android.dto.response.hyunmyeong.RoutineDetailResponse
+
+class HyunMyeongRepository() {
+
+    suspend fun fetchRoutineDetail(id: Int): RoutineDetailResponse =
+        hyunMyeongApi.fetchRoutineDetail(id)
 }
